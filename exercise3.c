a2460a81758a88142c586b396085841f19fc12cb
ibukie2g <ibuki.sugaya@gmail.com>
2025-07-03 13:14:47 +0900

    Dice game: initial revision

---
+++ exercise3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    printf("Rolling dice...\n");
+    int die1 = rand() % 6 + 1;
+    int die2 = rand() % 6 + 1;
+
+    printf("Die 1: %d\n", die1);
+    printf("Die 2: %d\n", die2);
+    printf("Total value: %d\n", die1 + die2);
+
+    return 0;
+}
